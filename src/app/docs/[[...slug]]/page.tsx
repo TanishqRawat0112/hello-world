aabe834d08a6cfad8f05613502fdd8d5a1538141
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-05-23 10:59:52 +0530

    dynamic_routes

---
+++ src/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,20 @@
+export default function Docs({params}:{params:{
+    slug:string[];
+}}){
+            if(params.slug?.length===1){
+                return <h2>
+                    Document's Feature {params.slug[0]}:
+                </h2>
+            }
+            else if(params.slug?.length===2){
+                return <h2>
+                    Document's Feature {params.slug[0]}'s Concept {params.slug[1]}:
+                </h2>
+            }
+            else if(params.slug?.length===3){
+                return <h2>
+                    Document's Feature {params.slug[0]}'s Concept {params.slug[1]}'s example {params.slug[2]}:
+                </h2>
+            }
+            return <h1>Doc's Home Page : </h1>
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,10 @@
+import {notFound} from "next/navigation"
+
 export default function ProductDetails( {params}:{
     params:{reviewId:string,productId:string}
 } ){
+    if(parseInt(params.reviewId)>1000){
+        return notFound()
+    }
     return <h1>Review {params.reviewId} for the product {params.productId} : </h1>
 }
