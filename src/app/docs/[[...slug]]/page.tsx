c7384cb37c2ef54c29808b622fb7b4368a64ce49
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-05-24 18:06:23 +0530

    learning parallel routes and error handling

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -3,18 +3,18 @@ export default function Docs({params}:{params:{
 }}){
             if(params.slug?.length===1){
                 return <h2>
-                    Document's Feature {params.slug[0]}:
+                    Documents Feature {params.slug[0]}:
                 </h2>
             }
             else if(params.slug?.length===2){
                 return <h2>
-                    Document's Feature {params.slug[0]}'s Concept {params.slug[1]}:
+                    Documents Feature {params.slug[0]}s Concept {params.slug[1]}:
                 </h2>
             }
             else if(params.slug?.length===3){
                 return <h2>
-                    Document's Feature {params.slug[0]}'s Concept {params.slug[1]}'s example {params.slug[2]}:
+                    Documents Feature {params.slug[0]}s Concept {params.slug[1]}s example {params.slug[2]}:
                 </h2>
             }
-            return <h1>Doc's Home Page : </h1>
+            return <h1>Docs Home Page : </h1>
 }
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,16 @@
+"use client";
 import {notFound} from "next/navigation"
 
+function getRandomInt(count:number){
+    return Math.floor(Math.random() * count);
+}
 export default function ProductDetails( {params}:{
     params:{reviewId:string,productId:string}
 } ){
+    // const random = getRandomInt(2);
+    // if(random===1){
+    //     throw new Error("Error loading Review");
+    // }
     if(parseInt(params.reviewId)>1000){
         return notFound()
     }
